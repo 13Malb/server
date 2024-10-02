b3439768dae77ca7efa7cef293b8d05202a1598c
Mongezi <mongezi1325@outlook.com>
2024-10-02 11:50:08 +0200

    Add files via upload

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('v1');
   app.use(helmet());
-  app.enableCors();
+  app.enableCors(
+    {
+      origin:['server.dailydash.co.za',
+        'http://localhost:3000',
+      'dailydash.co.za']
+    }
+  );
   await app.listen(4000);
 }
 bootstrap();
